64970315cda09e5692b6b70a2a43befe1b0d14c3
zancientstory <zancientstory@gmail.com>
2024-03-25 21:42:52 +0800

    add learn_crc

---
+++ F1_series/learn_crc/MDK-ARM/learn_crc/stm32f1xx_hal_gpio_ex.d
@@ -0,0 +1,29 @@
+learn_crc/stm32f1xx_hal_gpio_ex.o: \
+  ..\Drivers\STM32F1xx_HAL_Driver\Src\stm32f1xx_hal_gpio_ex.c \
+  ..\Drivers\STM32F1xx_HAL_Driver\Inc\stm32f1xx_hal.h \
+  ..\Core\Inc\stm32f1xx_hal_conf.h \
+  ..\Drivers\STM32F1xx_HAL_Driver\Inc\stm32f1xx_hal_rcc.h \
+  ..\Drivers\STM32F1xx_HAL_Driver\Inc\stm32f1xx_hal_def.h \
+  ..\Drivers\CMSIS\Device\ST\STM32F1xx\Include\stm32f1xx.h \
+  ..\Drivers\CMSIS\Device\ST\STM32F1xx\Include\stm32f103xb.h \
+  ..\Drivers\CMSIS\Include\core_cm3.h \
+  C:\Program\ Files\ (x86)\keil5\ARM\ARMCLANG\include\stdint.h \
+  C:\worker\RM\EC\ Code\F1_series\learn_crc\Drivers\CMSIS\Include\cmsis_version.h \
+  C:\worker\RM\EC\ Code\F1_series\learn_crc\Drivers\CMSIS\Include\cmsis_compiler.h \
+  C:\worker\RM\EC\ Code\F1_series\learn_crc\Drivers\CMSIS\Include\cmsis_armclang.h \
+  C:\Program\ Files\ (x86)\keil5\ARM\ARMCLANG\include\arm_compat.h \
+  C:\Program\ Files\ (x86)\keil5\ARM\ARMCLANG\include\arm_acle.h \
+  ..\Drivers\CMSIS\Device\ST\STM32F1xx\Include\system_stm32f1xx.h \
+  ..\Drivers\STM32F1xx_HAL_Driver\Inc\Legacy\stm32_hal_legacy.h \
+  C:\Program\ Files\ (x86)\keil5\ARM\ARMCLANG\include\stddef.h \
+  ..\Drivers\STM32F1xx_HAL_Driver\Inc\stm32f1xx_hal_rcc_ex.h \
+  ..\Drivers\STM32F1xx_HAL_Driver\Inc\stm32f1xx_hal_gpio.h \
+  ..\Drivers\STM32F1xx_HAL_Driver\Inc\stm32f1xx_hal_gpio_ex.h \
+  ..\Drivers\STM32F1xx_HAL_Driver\Inc\stm32f1xx_hal_exti.h \
+  ..\Drivers\STM32F1xx_HAL_Driver\Inc\stm32f1xx_hal_dma.h \
+  ..\Drivers\STM32F1xx_HAL_Driver\Inc\stm32f1xx_hal_dma_ex.h \
+  ..\Drivers\STM32F1xx_HAL_Driver\Inc\stm32f1xx_hal_cortex.h \
+  ..\Drivers\STM32F1xx_HAL_Driver\Inc\stm32f1xx_hal_crc.h \
+  ..\Drivers\STM32F1xx_HAL_Driver\Inc\stm32f1xx_hal_flash.h \
+  ..\Drivers\STM32F1xx_HAL_Driver\Inc\stm32f1xx_hal_flash_ex.h \
+  ..\Drivers\STM32F1xx_HAL_Driver\Inc\stm32f1xx_hal_pwr.h
